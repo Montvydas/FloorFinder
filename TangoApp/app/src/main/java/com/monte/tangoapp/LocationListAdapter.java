92bf27d72faf4e725014d8217022f54d2c2d92ee
Monty Klumbys <mklumbys@novarumdx.com>
2016-06-12 16:03:55 +0100

    Added barometer app code, also exporting data to csv

---
+++ TangoApp/app/src/main/java/com/monte/tangoapp/LocationListAdapter.java
@@ -0,0 +1,87 @@
+package com.monte.tangoapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by monty on 12/06/2016.
+ */
+public class LocationListAdapter extends BaseAdapter{
+//    private String[] location;
+//    private String[] altitude;
+//    private String[] pressure;
+    private List location;
+    private List altitude;
+    private List pressure;
+    Context context;
+
+
+    private static LayoutInflater inflater=null;
+
+    public LocationListAdapter (MainActivity mainActivity, List location, List altitude, List pressure){
+        this.location = location;
+        this.altitude = altitude;
+        this.pressure = pressure;
+        this.context = mainActivity;
+        inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void add (String location, String pressure, String altitude){
+        this.location.add(location);
+        this.pressure.add(pressure);
+        this.altitude.add(altitude);
+    }
+
+    public class Holder
+    {
+        TextView location;
+        TextView pressure;
+        TextView altitude;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Holder holder=new Holder();
+        View rowView;
+        rowView = inflater.inflate(R.layout.location_list_adapter_view, null);
+        holder.location=(TextView) rowView.findViewById(R.id.locationText);
+        holder.pressure=(TextView) rowView.findViewById(R.id.pressureText);
+        holder.altitude=(TextView) rowView.findViewById(R.id.altitudeText);
+        holder.location.setText(location.get(position).toString());
+        holder.pressure.setText(pressure.get(position).toString());
+        holder.altitude.setText(altitude.get(position).toString());
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(context, "You Clicked " + location.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+        return null;
+    }
+
+    //    private List locationList
+}
--- TangoApp/app/src/main/java/com/monte/tangoapp/MainActivity.java
+++ TangoApp/app/src/main/java/com/monte/tangoapp/MainActivity.java
@@ -1,19 +1,45 @@
 package com.monte.tangoapp;
 
+import android.Manifest;
+import android.app.LauncherActivity;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+
 import static android.hardware.SensorManager.getAltitude;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, ListView.OnItemLongClickListener{
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
 
     private SensorManager sensorManager;
     private Sensor pressureSensor;
@@ -21,12 +47,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView currentPressureText;
     private TextView currentAltitudeText;
     private TextView altitudeDifferenceText;
+    private EditText currentLocation;
+    private ArrayAdapter locationListAdapter;
+//    private List locationList;
+
+    List altitudeList = new ArrayList();
+    List locationList = new ArrayList();
+    List pressureList = new ArrayList();
+
+    List displayTextList = new ArrayList();
+
+
+    private LocationListAdapter customLocationListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialiseSensors();
         addViews();
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void initialiseSensors (){
@@ -46,6 +85,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         currentPressureText = (TextView) findViewById(R.id.currentPressureText);
         currentAltitudeText = (TextView) findViewById(R.id.currentAltitudeText);
         altitudeDifferenceText = (TextView) findViewById(R.id.altitudeDifferenceText);
+        currentLocation = (EditText) findViewById(R.id.currentLocation);
+
+//        customLocationListAdapter = new LocationListAdapter(this, locationList, pressureList, altitudeList);
+
+        locationListAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,displayTextList);
+        ListView locationListView = (ListView) findViewById(R.id.locationList);
+        locationListView.setOnItemLongClickListener(this);
+//        locationListView.setAdapter(customLocationListAdapter);
+        locationListView.setAdapter(locationListAdapter);
     }
 
     @Override
@@ -65,7 +113,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         switch (event.sensor.getType()){
             case Sensor.TYPE_PRESSURE:
                 millibars_of_pressure = event.values[0];
-                Log.e("Pressure is", "" + millibars_of_pressure);
+//                Log.e("Pressure is", "" + millibars_of_pressure);
                 float altitude = getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, millibars_of_pressure);
                 currentAltitudeText.setText(String.format("%.3f m", altitude));
                 currentPressureText.setText(String.format("%.3f mbar", millibars_of_pressure ));
@@ -81,19 +129,204 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             case R.id.firstPointButton:
                 firstPointAltitude = getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, millibars_of_pressure);
                 altitudeDifferenceText.setText("0 m");
-                Toast.makeText(this, "1st Point Collected!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "1st Point " + String.format("%.3f m", firstPointAltitude), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.secondPointButton:
                 secondPointAltitude = getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, millibars_of_pressure);
-                Toast.makeText(this, "2st Point Collected!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "2nd Point " + String.format("%.3f m", firstPointAltitude), Toast.LENGTH_SHORT).show();
                 float diff = secondPointAltitude - firstPointAltitude;
                 altitudeDifferenceText.setText(String.format("%.3f m", diff));
                 break;
         }
     }
 
+    public void addLocation (View view){
+        if (currentLocation.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Add location name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        float altitude = getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, millibars_of_pressure);
+//        customLocationListAdapter.add(currentLocation.getText().toString(),
+//                String.format(" %.3f m", millibars_of_pressure),
+//                String.format(" %.3f m", altitude));
+//        customLocationListAdapter.notifyDataSetChanged();
+
+        locationList.add(currentLocation.getText().toString());
+        pressureList.add(String.format("%.3f", millibars_of_pressure));
+        altitudeList.add(String.format("%.3f", altitude));
+
+        locationListAdapter.add(currentLocation.getText().toString()
+                + String.format("  %.3f mbar ", millibars_of_pressure)
+                + String.format(" %.3f m", altitude));
+        locationListAdapter.notifyDataSetChanged();
+
+        currentLocation.getText().clear();
+    }
+
+    public void removeAllLocations (View view){
+        if (locationListAdapter.isEmpty()) {
+            Toast.makeText(this, "Nothing to Delete", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        AlertDialog.Builder adb=new AlertDialog.Builder(MainActivity.this);
+        adb.setTitle("Delete All Locations?");
+        adb.setNegativeButton("Cancel", null);
+        adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                locationList.clear();
+                pressureList.clear();
+                altitudeList.clear();
+                displayTextList.clear();
+                locationListAdapter.clear();
+                locationListAdapter.notifyDataSetChanged();
+            }});
+        adb.show();
+
+    }
+
+    public void exportLocations (View view){
+        if (locationListAdapter.isEmpty()) {
+            Toast.makeText(this, "Nothing to Export", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+//            } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Folder Name");
+
+        // Set up the input
+        final EditText folderLocation = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        folderLocation.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(folderLocation);
+
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                exportToFile (folderLocation.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+//                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+
+    }
+
+    private void exportToFile (String folderName){
+        // Here, thisActivity is the current activity
+
+        String dataString = new String();
+        for (int i = 0; i < locationList.size(); i++){
+            dataString += "\"" +  locationList.get(i) +"\",\"" + pressureList.get(i) + "\",\"" + altitudeList.get(i) + "\"\n";
+        }
+
+
+        String columnString =   "\"Location\",\"Pressure (mbar)\",\"Altitude (m)\"";
+        String combinedString = columnString + "\n" + dataString;
+
+        Log.e("locations", combinedString);
+        Log.e("FolderName", folderName);
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy_HH:mm");
+        String formattedDate = df.format(c.getTime());
+
+        Log.e("data", formattedDate);
+
+        File file   = null;
+        File root   = Environment.getExternalStorageDirectory();
+
+        System.out.println(root);
+
+
+        if (root.canWrite()){
+            File dir    =   new File (root.getAbsolutePath() + "/TangoApp/" + folderName + "/");
+            dir.mkdirs();
+
+            file   =   new File(dir, formattedDate + ".csv");
+            FileOutputStream out   =   null;
+            try {
+                out = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.write(combinedString.getBytes());
+                Toast.makeText(this, "Stored in /TangoApp/" + folderName + "/" + formattedDate + ".csv", Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        AlertDialog.Builder adb=new AlertDialog.Builder(MainActivity.this);
+        adb.setTitle("Delete?");
+        adb.setMessage("Are you sure you want to delete \"" + displayTextList.get(position) + "\"?");
+        final int positionToRemove = position;
+        adb.setNegativeButton("Cancel", null);
+        adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                locationList.remove(positionToRemove);
+                pressureList.remove(positionToRemove);
+                altitudeList.remove(positionToRemove);
+
+                displayTextList.remove(positionToRemove);
+                locationListAdapter.notifyDataSetChanged();
+            }});
+        adb.show();
+        return false;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+        }
     }
 }
